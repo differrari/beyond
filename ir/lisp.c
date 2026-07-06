a2cc7c2d8ce4a07c16f3b872b2812a70ed358905
di <git@diferrari.com>
2026-07-06 00:00:00 +0000

    [IR] generalized lisp ir generator

--- ir/lisp.c
+++ ir/lisp.c
@@ -1,33 +1,28 @@
 #include "lisp.h"
 #include "syscalls/syscalls.h"
 #include "ir/general.h"
+#include "codegen/code_generator.h"
 
 ast_node* next_node(stack_navigator *sn){
     ast_node *node = chunk_array_get(sn->stack, sn->cursor++);
     return node;
 }
 
-extern void lisp_val_code_register_elem(void *ptr, int type, Token elem);
-extern void s_exp_code_register_subrule(void *ptr, int type, codegen child);
-
-void make_ir_from_node(stack_navigator *sn, int depth, s_exp_code *code){
+void make_ir_from_node(stack_navigator *sn, int depth, codegen code){
     ast_node *node = next_node(sn);
     while (node){
+        if (node->t.kind && node->sem_value){
+            codegen_register_elem(code, node->sem_value, node->t);
+        }
         if (depth >= 0 && node->depth <= depth){
             // print(")");
             return;
         }
-        if (node->sem_value == sem_rule_sexp){
-            // print("(");
-            codegen new_code = s_exp_code_init();
-            make_ir_from_node(sn,node->depth, new_code.ptr);
-            s_exp_code_register_subrule(code, 0, new_code);
-        } else if (node->sem_value) {
-            ast_node *tok_node = next_node(sn);
+        if (node->sem_value) {
             // print("[%s] %v",sem_rule_strings[tok_node->sem_value],token_to_slice(tok_node->t));
-            codegen new_atom = lisp_val_code_init();
-            lisp_val_code_register_elem(new_atom.ptr, 0, tok_node->t);
-            s_exp_code_register_subrule(code, 0, new_atom);
+            codegen new_atom = begin_rule(node->sem_value);
+            make_ir_from_node(sn,node->depth, new_atom);
+            codegen_register_subrule(code, node->sem_value, new_atom);
         }
         node = next_node(sn);
     }
@@ -38,6 +33,6 @@ codegen gen_ir_lisp(chunk_array_t *stack){
     codegen root = s_exp_code_init();
     root.type = sem_rule_sexp;
     stack_navigator nav = make_stack_navigator(stack);
-    make_ir_from_node(&nav, -1, root.ptr);
+    make_ir_from_node(&nav, -1, root);
     return root;
 }
--- test.lisp
+++ test.lisp
@@ -1,1 +1,4 @@
-(cons 1 (quote (add 2 2)))
+(cons (add 12 2) (quote (add 2 2)))
+(add 12 2)
+
+(add 23 2)
