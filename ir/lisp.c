ada9892a5e4c4e645fccd780a44d001fc4144acc
di <git@diferrari.com>
2026-07-06 00:00:00 +0000

    [IR] propagate depth exit upwards

--- ir/lisp.c
+++ ir/lisp.c
@@ -8,25 +8,25 @@ ast_node* next_node(stack_navigator *sn){
     return node;
 }
 
-void make_ir_from_node(stack_navigator *sn, int depth, codegen code){
+int make_ir_from_node(stack_navigator *sn, int depth, codegen code){
     ast_node *node = next_node(sn);
     while (node){
-        if (node->t.kind && node->sem_value){
+        if (node->t.kind && node->sem_value && node->depth == depth){
             codegen_register_elem(code, node->sem_value, node->t);
         }
-        if (depth >= 0 && node->depth <= depth){
-            // print(")");
-            return;
+        else if (depth >= 0 && node->depth <= depth){
+            return node->depth;
         }
-        if (node->sem_value) {
-            // print("[%s] %v",sem_rule_strings[tok_node->sem_value],token_to_slice(tok_node->t));
+        else if (node->sem_value) {
             codegen new_atom = begin_rule(node->sem_value);
-            make_ir_from_node(sn,node->depth, new_atom);
+            int d = make_ir_from_node(sn,node->depth, new_atom);
             codegen_register_subrule(code, node->sem_value, new_atom);
+            if (depth >= 0 && d <= depth)
+                return d;
         }
         node = next_node(sn);
     }
-    // if (depth >= 0) print(")");
+    return 0;
 }
 
 codegen gen_ir_lisp(chunk_array_t *stack){
